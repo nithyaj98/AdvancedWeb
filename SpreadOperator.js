76df4712d16adaca0593d9509137bf211fb44caf
Nithya <nithyaj1998@gmail.com>
2020-02-15 10:54:12 +0530

    my first commit

---
+++ SpreadOperator.js
@@ -0,0 +1,12 @@
+var morning=
+{
+    breakfast:"idly,chutney,coffee",
+    lunch:"chapati,dal,curds"
+}
+var dinner="rice and sambar"
+var backpackingMeals=
+{
+    ...morning,
+    dinner
+}
+console.log(backpackingMeals)
---
+++ sandwich.js
@@ -0,0 +1,7 @@
+var sandwich=
+{
+    bread:"soft",
+    cheese:"Cheddar",
+    toppings:["cucumber","tomato","pineapple"]
+}
+var{bread,cheese}=sandwichconsole.log(bread,cheese)
